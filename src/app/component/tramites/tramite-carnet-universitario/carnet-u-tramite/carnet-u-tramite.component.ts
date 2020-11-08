aba456cc941c8d6281f7c97f89d859e6b3df85d0
Fernando Callasaca <fernandocallasaca@outlook.com>
2020-11-07 19:36:15 -0500

    vista de tramite actualizada a un 70%

--- src/app/component/tramites/tramite-carnet-universitario/carnet-u-tramite/carnet-u-tramite.component.ts
+++ src/app/component/tramites/tramite-carnet-universitario/carnet-u-tramite/carnet-u-tramite.component.ts
@@ -1,13 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms'; // Para utilizar un formulario reactivo
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+
+import { GeneralService } from './../../../../service/general.service';
+import { BaseComponent } from './../../../base/base.component';
+import { MatSnackBar } from '@angular/material';
+import { Router } from '@angular/router';
+import { AppSettings } from 'src/app/common/appsettings';
 
 @Component({
   selector: 'app-carnet-u-tramite',
   templateUrl: './carnet-u-tramite.component.html',
-  styleUrls: ['./carnet-u-tramite.component.css']
+  styleUrls: ['./carnet-u-tramite.component.css'],
+  providers: [GeneralService]
 })
-export class CarnetUTramiteComponent implements OnInit {
+export class CarnetUTramiteComponent extends BaseComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public snackBar: MatSnackBar,
+    public router: Router,
+    private generalService: GeneralService,
+    private formBuilder: FormBuilder
+  ) {
+    super(snackBar, router);
+  }
 
   ngOnInit() {
   }
